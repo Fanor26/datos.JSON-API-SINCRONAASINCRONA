4f8a256f41fafda09263aabb672fd1f208b5c99e
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-03-29 02:08:44 -0400

    basededatos

---
+++ src/database.js
@@ -0,0 +1,17 @@
+//Creamos nuestra base de datos
+
+const mongoose = require('mongoose')
+
+URI =''
+
+
+
+
+mongoose.connect(URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+})
+.then (db => console.log('Base de datos conectado', db.connection.name)).catch(error => console.log(error))
+
+module.exports= mongoose
---
+++ src/index.js
@@ -0,0 +1,24 @@
+// Requerimos las dependencias necesarias
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const dotenv = require('dotenv'); 
+const cors = require('cors')
+const bodyparser = require('body-parser')
+require('./database')
+
+
+dotenv.config();
+//Configuracion del puerto
+
+app.set('Port',process.env.PORT || 5000);
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+app.use(cors({origen: '*'}));
+
+
+app.listen(app.get('Port'),()=>{
+  console.log('Servidor escuchando por el puerto',app.get('Port'))
+ })
+ 
